84ee6b9bf291f8df3a4f32087983d4e69d709aed
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2023-01-12 22:14:56 +0800

    Add files via upload

---
+++ volume tabung.py
@@ -0,0 +1,9 @@
+#program menentukan volume tabung
+
+a = int(input("masukkan diameter alas tabung : "))
+t = int(input("masukkan tinggi tabung  : "))
+r = a / 2 
+v = 22/7 * r**2 q*t
+
+print("volume tabung",v)
+
